6909d3f8d3ea2975b4b56be8edb98c67afcce4d0
Navin1012 <nc13760@gmail.com>
2026-06-17 17:37:07 +0530

    add doc api

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,10 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\UserProfileController;
 use App\Http\Controllers\Api\CompanyController;
 use App\Http\Controllers\Api\CompanyBankAccountController;
+use App\Http\Controllers\Api\MasterDataController;
+use App\Http\Controllers\Api\LeadController;
+
+
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -28,6 +32,11 @@ Route::middleware(['auth:api', 'role:user'])->group(function () {
     Route::get('/user/company/banks', [CompanyBankAccountController::class, 'index']);
     Route::post('/user/company/banks', [CompanyBankAccountController::class, 'store']);
     Route::delete('/user/company/banks/{id}', [CompanyBankAccountController::class, 'destroy']);
+
+    Route::get('/master/loan-types', [MasterDataController::class, 'getLoanTypes']);
+    Route::get('/master/documents', [MasterDataController::class, 'getRequiredDocuments']);
+
+    Route::post('/user/loan/apply', [LeadController::class, 'applyForLoan']);
 });
 
 
